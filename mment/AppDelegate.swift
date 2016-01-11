87e00e9fa3a9a7bab0a652573c49a9196c1d8255
noga.highland@googlemail.com <noga.highland@googlemail.com>
2016-01-12 00:48:50 +0900

    background fetch

--- mment/AppDelegate.swift
+++ mment/AppDelegate.swift
@@ -16,7 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         musicTracker = MmentTracker()
+        UIApplication.sharedApplication().setMinimumBackgroundFetchInterval(UIApplicationBackgroundFetchIntervalMinimum)
         return true
+    }
+    
+    func application(application: UIApplication, performFetchWithCompletionHandler completionHandler: (UIBackgroundFetchResult) -> Void) {
+        print(NSDate.init())
+        completionHandler(UIBackgroundFetchResult.Failed)
     }
 
     func applicationWillResignActive(application: UIApplication) {
